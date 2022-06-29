7f8280e948a75f5ab831e2dfd2eb53508d0ac487
Håvard Brenne <hava.brenne@gmail.com>
2022-06-29 07:53:36 +0200

    Fix bugs for multi action envs

--- PPO.py
+++ PPO.py
@@ -2,6 +2,8 @@ from cmath import nan
 import yaml
 import isaacgym
 
+import os
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,6 +12,7 @@ from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 
 from tasks.cartpole import Cartpole
+from isaacgymenvs.tasks import isaacgym_task_map
 
 def orthogonal_init(layer):
     if isinstance(layer, nn.Linear):
@@ -18,27 +21,27 @@ def orthogonal_init(layer):
     
 class ActorCritic(nn.Module):
 
-    def __init__(self, n_obs, n_actions):
+    def __init__(self, n_obs, n_actions, device):
         super(ActorCritic, self).__init__()
         #Outputs mean value of action
         self.actor = nn.Sequential(
-            nn.Linear(n_obs, 64),
+            nn.Linear(n_obs, 256),
             nn.Tanh(),
-            nn.Linear(64, 64),
+            nn.Linear(256, 256),
             nn.Tanh(),
-            nn.Linear(64, n_actions)
+            nn.Linear(256, n_actions)
         )
         self.actor.apply(orthogonal_init)
         #We want the variance elements to be trainable, assuming no cross correlation
-        self.actor_variance = torch.full((n_actions,1), 0.15, device=device)
+        self.actor_variance = torch.full((n_actions,), 0.15, device=device)
         
         #Outputs value function
         self.critic = nn.Sequential(
-            nn.Linear(n_obs, 64),
+            nn.Linear(n_obs, 256),
             nn.Tanh(),
-            nn.Linear(64,64),
+            nn.Linear(256,256),
             nn.Tanh(),
-            nn.Linear(64, 1)
+            nn.Linear(256, 1)
         )
         self.critic.apply(orthogonal_init)
 
@@ -51,20 +54,23 @@ class ActorCritic(nn.Module):
             return action, log_prob
     
         
-def select_action_normaldist(mean, variance):
-    prob_dist = torch.distributions.Normal(mean, variance)
+def select_action_normaldist(mean, variance, num_envs):
+    variance_matrix = variance.repeat((1,num_envs), num_envs).reshape(num_envs,variance.shape[0])
+    prob_dist = torch.distributions.Normal(mean, variance_matrix)
     action = prob_dist.sample()
     return action, prob_dist
-def select_action_multinormaldist(mean, variance):
-    covariance = torch.diag(variance)
-    prob_dist = torch.distributions.MultivariateNormal(mean, covariance)
+def select_action_multinormaldist(mean, variance, num_envs):
+    #variance_matrix = variance.repeat((1,num_envs), num_envs).reshape(num_envs,variance.shape[0])
+    #covariance_matrix = torch.diag(variance_matrix).unsqueeze(dim=0)
+    covariance_matrix = torch.diag(variance)
+    prob_dist = torch.distributions.MultivariateNormal(mean, covariance_matrix)
     action = prob_dist.sample()
     return action, prob_dist
 
 class PPO():
     def __init__(self):
         #Load task/env specific config
-        env = "Cartpole"
+        env = "Ant"
         with open("cfg/"+env+".yaml", 'r') as stream:
             try:
                 cfg=yaml.safe_load(stream)
@@ -72,27 +78,27 @@ class PPO():
             except yaml.YAMLError as exc:
                 print(exc)
         
-        self.vecenv = Cartpole(cfg, cfg["sim_device"], cfg["graphics_device_id"], 0)
-
-        self.num_obs = vecenv.cfg["env"]["numObservations"]
-        self.num_actions = vecenv.cfg["env"]["numActions"]
+        #self.vecenv = Cartpole(cfg, cfg["sim_device"], cfg["graphics_device_id"], 0)
+        self.vecenv = isaacgym_task_map[env](cfg, cfg["sim_device"], cfg["graphics_device_id"], 0)
+        self.num_obs = self.vecenv.cfg["env"]["numObservations"]
+        self.num_actions = self.vecenv.cfg["env"]["numActions"]
         self.num_envs = cfg["env"]["numEnvs"]
         self.device = cfg["rl_device"]
 
         #Hyperparams
 
         # Samples collected in total is num_envs*rollout_steps. minibatch_size should be a an integer factor of rollout_steps
-        self.rollout_steps = 100
-        self.minibatch_size = 25
+        self.rollout_steps = 50
+        self.minibatch_size = 10
 
-        self.num_epoch = 3
-        self.l_rate = 1e-4 
+        self.num_epoch = 5
+        self.l_rate = 3e-4 
         self.gamma = 0.99 #Reward discount factor
         self.lambda_ = 0.95 #GAE tuner
         self.epsilon = 0.3 #Clip epsilon
 
-        self.ac = ActorCritic(num_obs,num_actions).to(device)
-        self.optimizer = torch.optim.Adam(ac.parameters(), lr=l_rate)
+        self.ac = ActorCritic(self.num_obs, self.num_actions, self.device).to(self.device)
+        self.optimizer = torch.optim.Adam(self.ac.parameters(), lr=self.l_rate)
 
         self.tb = SummaryWriter()
 
@@ -103,7 +109,7 @@ class PPO():
         #We need rollout_step+1 values for advantage estimation
         self.value_buf = torch.zeros((self.rollout_steps+1, self.num_envs), device=self.device, dtype=torch.float)
         self.action_buf = torch.zeros((self.rollout_steps, self.num_envs, self.num_actions), device=self.device, dtype=torch.float)
-        self.log_prob_buf = torch.zeros((self.rollout_steps, self.num_envs,self.num_actions), device=self.device, dtype=torch.float)
+        self.log_prob_buf = torch.zeros((self.rollout_steps, self.num_envs), device=self.device, dtype=torch.float)
 
         #Buffers for last operation
         self.next_obs_buf = torch.zeros((self.num_envs, self.num_obs), device=self.device, dtype=torch.float)
@@ -127,7 +133,7 @@ class PPO():
                 #Only inference, no grad needed
                 with torch.no_grad():
                     self.value_buf[step] = self.ac.critic(self.next_obs_buf).squeeze()
-                    action, prob_dist = select_action_normaldist(self.ac.actor(self.next_obs_buf), self.ac.actor_variance)
+                    action, prob_dist = select_action_multinormaldist(self.ac.actor(self.next_obs_buf), self.ac.actor_variance, self.num_envs)
                 self.log_prob_buf[step] = prob_dist.log_prob(action)
                 self.next_obs_dict, self.reward_buf[step], self.next_reset_buf, _ = self.vecenv.step(action)
                 self.action_buf[step] = action
@@ -161,7 +167,7 @@ class PPO():
                     np.random.shuffle(shuffled_indicies)
                     for mb in range(self.rollout_steps//self.minibatch_size):
                         minibatch_indicies = shuffled_indicies[mb*self.minibatch_size:(mb+1)*self.minibatch_size]
-                        actions, prob_dists = select_action_normaldist(self.ac.actor(self.obs_buf[minibatch_indicies]), self.ac.actor_variance)
+                        actions, prob_dists = select_action_multinormaldist(self.ac.actor(self.obs_buf[minibatch_indicies]), self.ac.actor_variance, self.num_envs)
                         log_probs_new = prob_dists.log_prob(actions)
                         
 
@@ -170,16 +176,16 @@ class PPO():
                         log_prob = self.log_prob_buf[minibatch_indicies]
                         returns = return_buf[minibatch_indicies]
                         advantage = gae_buf[minibatch_indicies]
-                        self.update_net(log_prob, log_probs_new, values, returns, advantage, ac, tb, global_step)
+                        self.update_net(log_prob, log_probs_new, values, returns, advantage)
                     
             
 
             self.ac.actor_variance *= 0.9
 
             print("Timestep" + str(self.global_step) + ": Score: " + str(score.data) + ", Action Variance: " + str(self.ac.actor_variance.data.mean()))
-            tb.add_scalar("Advantage", gae_buf.mean(), self.global_step)
+            self.tb.add_scalar("Advantage", gae_buf.mean(), self.global_step)
 
-    def update_net(self, log_prob, log_prob_new, values, returns, advantage, ac, tb, global_step):
+    def update_net(self, log_prob, log_prob_new, values, returns, advantage):
         #Calculate loss function for actor network
         r = torch.exp(log_prob_new - log_prob).squeeze()
         # Loss negated as we want SGD
@@ -195,13 +201,14 @@ class PPO():
         
         self.optimizer.zero_grad()
         tot_loss.mean().backward()
-        nn.utils.clip_grad_norm_(ac.parameters(), 1.0)
+        nn.utils.clip_grad_norm_(self.ac.parameters(), 0.5)
         self.optimizer.step()
 
-        self.tb.add_scalar("Loss/total", tot_loss, global_step)
-        self.tb.add_scalar("Loss/Actor", actor_loss, global_step)
-        self.tb.add_scalar("Loss/Critic", critic_loss, global_step)
+        self.tb.add_scalar("Loss/total", tot_loss, self.global_step)
+        self.tb.add_scalar("Loss/Actor", actor_loss, self.global_step)
+        self.tb.add_scalar("Loss/Critic", critic_loss, self.global_step)
 
 if __name__ == "__main__":
+    #os.chdir()
     learner = PPO()
     learner.run()
